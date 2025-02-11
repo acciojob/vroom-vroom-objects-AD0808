344c747a0c76851d7861fb37647d834945b76a1d
AD0808 <108204118+AD0808@users.noreply.github.com>
2025-02-11 10:08:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-// Car constructor function
-function Car(make, model) {
+class Car{
+	constructor(make, model) {
     this.make = make;
     this.model = model;
+	}
 }
 
-// Adding method to Car prototype
-Car.prototype.getMakeModel = function() {
-    return `${this.make} ${this.model}`;
-};
-
-// SportsCar constructor function
-function SportsCar(make, model, topSpeed) {
-    // Call the Car constructor with this context
-    Car.call(this, make, model);
+class SportsCar{
+	constructor(make, model, topSpeed) {
+    super(make, model);
     this.topSpeed = topSpeed;
+	}
+
+	function getTopSpeed(){
+		return topSpeed;
+	}
 }
 
-// Inherit from Car prototype
-SportsCar.prototype = Object.create(Car.prototype);
 
-// Ensure the constructor property is set correctly
-SportsCar.prototype.constructor = SportsCar;
 
-// Adding method to SportsCar prototype
-SportsCar.prototype.getTopSpeed = function() {
-    return this.topSpeed;
-};
+
+
+// SportsCar.prototype = Object.create(Car.prototype);
+
+// SportsCar.prototype.constructor = SportsCar;
+
+// SportsCar.prototype.getTopSpeed = function() {
+//     return this.topSpeed;
+// };
 
 
 // Do not change the code below
